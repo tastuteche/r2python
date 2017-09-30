ad5726ddfb58453f23ce0fc30fbb84fff3f34fa6
tastu teche <tastuteche@yahoo.com>
2017-09-30 15:43:41 +0800

    r to python demo worked

--- r2python/r2python.py
+++ r2python/r2python.py
@@ -5,22 +5,83 @@
 # A simple calculator with variables -- all in one file.
 # -----------------------------------------------------------------------------
 
-tokens = (
+tokens = [
     'NAME', 'NUMBER',
     'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS',
-    'LPAREN', 'RPAREN',
-)
+    'LPAREN', 'RPAREN', 'NUM_CONST'
+]
+
+reserved = {
+    "NULL": "NULL_CONST",
+    #    "NA": "NUM_CONST",
+    #    "TRUE": "NUM_CONST",
+    #    "FALSE": "NUM_CONST",
+    #    "Inf": "NUM_CONST",
+    #    "NaN": "NUM_CONST",
+    #    "NA_integer_": "NUM_CONST",
+    #    "NA_real_": "NUM_CONST",
+    #    "NA_character_": "NUM_CONST",
+    #    "NA_complex_": "NUM_CONST",
+    "function": "FUNCTION",
+    "while": "WHILE",
+    "repeat": "REPEAT",
+    "for": "FOR",
+    "if": "IF",
+    "in": "IN",
+    "else": "ELSE",
+    "next": "NEXT",
+    "break": "BREAK",
+    "...": "SYMBOL",
+}
+t_NUM_CONST = r'NA|TRUE|FALSE|Inf|NaN|NA_integer_|NA_real_|NA_character_|NA_complex_'
+tokens += reserved.values()
+
 
 # Tokens
+# t_c =  r'{' #brace
+#t_c =  r'('
+#t_c =  r'['
+#t_c =  r'?'
+#t_c =  r'*'
+
 
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_EQUALS = r'='
+#t_EQUALS = r'='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
+t_LE = r'<='
+#t_LEFT_ASSIGN = r'<-'
+#t_LEFT_ASSIGN = r'<<-'
+t_LT = r'<'
+t_GE = r'>='
+t_GT = r'>'
+t_NE = r'!='
+t_EQ = r'=='
+t_EQ_ASSIGN = r'='
+t_NS_GET_INT = r':::'
+t_NS_GET = r'::'
+t_LEFT_ASSIGN = r':=|<-|<<-'
+t_AND2 = r'&&'
+t_AND = r'&'
+t_OR2 = r'\|\|'
+t_OR = r'\|'
+t_LBB = r'\[\['
+
+tokens += ['NS_GET_INT', 'OR2', 'LEFT_ASSIGN', 'EQ', 'NS_GET', 'AND2',
+           'GE', 'LBB', 'NE', 'LE', 'GT', 'LT', 'AND', 'OR', 'EQ_ASSIGN']
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    if t.value in reserved:
+        t.type = reserved[t.value]
+    return t
 
 
 def t_NUMBER(t):
@@ -52,14 +113,42 @@ import ply.lex as lex
 lexer = lex.lex()
 
 # Parsing rules
+t_QUESTION = r'\?'
+t_TILDE_ = r'~'
+t_COLON = r':'
+t_CARET = r'\^'
+t_AT_SIGN = r'@'
+t_DOLLAR = r'\$'
+t_LBRACKET = r'\['
+
+tokens += ['RIGHT_ASSIGN', 'QUESTION', 'AT_SIGN', 'TILDE_', 'UNOT', 'LOW',
+           'SPECIAL', 'UPLUS', 'COLON', 'DOLLAR', 'LBRACKET', 'NOT', 'CARET', 'TILDE']
 
 precedence = (
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE'),
-    ('right', 'UMINUS'),
+    ("left", 'QUESTION'),
+    ("left", "LOW", "WHILE", "FOR", "REPEAT",),
+    ("right", "IF",),
+    ("left", "ELSE",),
+    ("right", "LEFT_ASSIGN",),
+    ("right", "EQ_ASSIGN",),
+    ("left", "RIGHT_ASSIGN",),
+    ("left", 'TILDE_', "TILDE",),
+    ("left", "OR", "OR2",),
+    ("left", "AND", "AND2",),
+    ("left", "UNOT", "NOT",),
+    ("nonassoc", "GT", "GE", "LT", "LE", "EQ", "NE",),
+    ("left", 'PLUS', 'MINUS'),
+    ("left", 'TIMES', 'DIVIDE'),
+    ("left", "SPECIAL",),
+    ("left", 'COLON'),
+    ("left", "UMINUS", "UPLUS",),
+    ("right", 'CARET'),
+    ("left", 'DOLLAR', 'AT_SIGN'),
+    ("left", "NS_GET", "NS_GET_INT",),
+    ("nonassoc", 'LPAREN', 'LBRACKET', "LBB",),
 )
 
-# dictionary of names
+# DICTIONARY OF names
 names = {}
 
 
@@ -78,14 +167,8 @@ def p_expression_binop(t):
                   | expression MINUS expression
                   | expression TIMES expression
                   | expression DIVIDE expression'''
-    if t[2] == '+':
-        t[0] = t[1] + t[3]
-    elif t[2] == '-':
-        t[0] = t[1] - t[3]
-    elif t[2] == '*':
-        t[0] = t[1] * t[3]
-    elif t[2] == '/':
-        t[0] = t[1] / t[3]
+    if t[2] in ['+', '-', '*', '/']:
+        t[0] = "%s %s %s" % (t[1], t[2], t[3])
 
 
 def p_expression_uminus(t):
@@ -100,7 +183,7 @@ def p_expression_group(t):
 
 def p_expression_number(t):
     'expression : NUMBER'
-    t[0] = t[1]
+    t[0] = str(t[1])
 
 
 def p_expression_name(t):
